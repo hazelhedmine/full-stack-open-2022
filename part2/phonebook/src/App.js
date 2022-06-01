2294b685f78fd7dcb40458c238630d464c8c918f
Hazel Tan <e0424860@u.nus.edu>
2022-06-01 13:04:29 +0800

    add exercise 2.9: the phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -29,18 +33,32 @@ const App = () => {
   };
 
   const handleNameChange = (event) => {
-    console.log("name input:>> ", event.target.value);
+    console.log("name input :>> ", event.target.value);
     setNewName(event.target.value);
   };
 
   const handleNumberChange = (event) => {
-    console.log("number input:>> ", event.target.value);
+    console.log("number input :>> ", event.target.value);
     setNewNumber(event.target.value);
   };
+
+  const handleFilterName = (event) => {
+    console.log("filter input :>> ", event.target.value);
+    setFilterName(event.target.value);
+  };
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().startsWith(filterName.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filterName} onChange={handleFilterName}></input>
+      </div>
+      <h2>Add a New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -55,7 +73,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <p key={person.name}>
             {person.name} {person.number}
           </p>
