5c06dcb089df9b2bf5f0d91c6c246a4c8507093e
Hazel Tan <e0424860@u.nus.edu>
2022-06-13 16:41:13 +0800

    add exercise 4.4: helper functions and unit tests step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sumWithInitial = blogs.reduce(
+    (prevBlog, currBlog) => prevBlog + currBlog.likes,
+    0
+  )
+
+  return sumWithInitial
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
