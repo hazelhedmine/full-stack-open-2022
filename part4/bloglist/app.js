d7897f161ff8798f68c96a3d8dc7ab464ffad8d7
Hazel Tan <e0424860@u.nus.edu>
2022-06-13 14:52:56 +0800

    add exercise 4.2: blog list step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const { info, error } = require('./utils/logger')
+
+info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    info('connected to MongoDB')
+  })
+  .catch((error) => {
+    error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
