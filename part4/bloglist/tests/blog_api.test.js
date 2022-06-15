f17d547c9c4ea7a4e3fb82cbd6fd0367ac2f6a4f
Hazel Tan <e0424860@u.nus.edu>
2022-06-15 18:01:48 +0800

    add exercise 4.9: blog list tests step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blogs added have id property', async () => {
+  const blogs = await helper.blogsInDb()
+
+  const ids = blogs.map((r) => r.id)
+  console.log(ids)
+  expect(ids).toBeDefined()
 })
 
 afterAll(() => {
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,27 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+]
+
+// to be used for checking blogs stored in the database
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map((blog) => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs,
+  blogsInDb,
+}
