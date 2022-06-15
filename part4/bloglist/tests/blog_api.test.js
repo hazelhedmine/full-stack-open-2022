a50ce4489f8d1bf5ce03ae136c23efa418fd1c91
Hazel Tan <e0424860@u.nus.edu>
2022-06-15 18:21:27 +0800

    add exercise 4.11: blog list step4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -53,6 +53,28 @@ test('blog is successfully added', async () => {
   expect(titles).toContain('test')
 })
 
+test('blog without likes added will default to 0', async () => {
+  const newBlog = {
+    title: 'blog without likes added will default to 0',
+    author: 'test',
+    url: 'test',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const blog = blogsAtEnd.filter(
+    (n) => n.title === 'blog without likes added will default to 0'
+  )
+  expect(blog[0].likes).toBe(0)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
